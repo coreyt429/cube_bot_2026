31b439ff0d88dfc15892fad1035dba203fe89caf
Corey Thornburg <coreyt@coreyt.com>
2026-01-12 23:47:19 -0600

    motion_test

---
+++ motion_test.py
@@ -0,0 +1,29 @@
+import logging
+from time import sleep
+
+from cube_bot import CubeBot
+from maestro import Servo
+from arm import Arm
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger("calibrate")
+logger.setLevel(logging.DEBUG)
+
+def up(bot, clockwise=True):
+    """Move up surface clockwise"""
+    bot.arms['r'].open()
+    bot.arms['l'].rotate(180)
+    bot.arms['r'].close()
+    if clockwise:
+        bot.arms['r'].rotate(90)
+    else:
+        bot.arms['r'].rotate(-90)
+    bot.arms['r'].open()
+    bot.arms['l'].rotate(-180)
+
+
+if __name__ == "__main__":
+    bot = CubeBot("calibration.json")
+    up(bot)
+    sleep(10)
+    up(bot, clockwise=False)   
