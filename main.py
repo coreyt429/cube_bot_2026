cbc96609d1c413a99a6ea2440ded71fa3b55d376
Corey Thornburg <coreyt@coreyt.com>
2026-01-20 23:58:47 -0600

    load function

--- main.py
+++ main.py
@@ -2,17 +2,35 @@
 Main.py not used yet.
 """
 from display import Display
-from menu import Menu, MenuItem
+from menu import Menu, Buttons
 from cube_bot import CubeBot
 
-def load_cube(bot: CubeBot) -> None:
+def load_cube(display, buttons: Buttons, bot: CubeBot) -> None:
     """Load cube into CubeBot"""
     bot.arms['l'].open()
     bot.arms['r'].open()
-    input("Place the cube in the holder and press Enter to continue...")
-    bot.arms['l'].close()
-    bot.arms['r'].close()
+    display.draw_message("Load Cube", ['', "Up: close  Down: open", "Select: exit"])
 
+    while True:
+        if buttons.wait(timeout=0.05):
+            for ev in buttons.read_events():
+                if ev.edge != "PRESS":
+                    continue
+                if ev.btn == "up":
+                    for arm in bot.arms.values():
+                        servo = arm.servos['open']
+                        current_qus = servo.qus
+                        servo.set_qus(current_qus + 4, wait=True)
+                elif ev.btn == "down":
+                    for arm in bot.arms.values():
+                        servo = arm.servos['open']
+                        current_qus = servo.qus
+                        servo.set_qus(current_qus - 4, wait=True)
+                elif ev.btn == "select":
+                    display.draw_message("Load Cube", ["Exit"])
+                    return
+
+                display.draw_message("Load Cube", ['', "Up: close  Down: open", "Select: exit"])
 
 def main():
     """Main logic"""
